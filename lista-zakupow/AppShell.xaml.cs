b848d1311a6385abb67427d5f8da95bb0c378fc2
Wojciech Koperski <koperski.wojciech.06@gmail.com>
2025-11-26 03:43:50 +0100

    Dodaj pliki projektów.

---
+++ lista-zakupow/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace lista_zakupow
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ lista-zakupow/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace lista_zakupow
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
